920a21d4599ab32a22ab6e34f845cbd40d7cc0da
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 15:59:15 +0530

    component4 task of spotify

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './Spotify/Component.css'
 import Component1 from './Spotify/Component1';
 import Component2 from './Spotify/Component2';
 import Component3 from './Spotify/Component3';
+import Component4 from './Spotify/Component4';
 function App() {
   return (
    <>
     <Component1></Component1>
     <Component2></Component2>
     <Component3></Component3>
+    <Component4></Component4>
    </>
   );
 }
-
 export default App;
