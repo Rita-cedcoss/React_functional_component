ac0a57ceaada5ede0a40be62a520570cd3a38bf1
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 16:09:04 +0530

    component task5 completed

---
+++ src/Spotify/Component5.js
@@ -0,0 +1,28 @@
+import React from 'react'
+export default function Component5() {
+  return (
+    <div id="component5">
+         <div id="comp5outer">
+          <h1>Questions</h1>
+          <p>We've got answers</p>
+         <div id="paraico">
+          <div><p>What is Spotify and Spotify Premium </p><p class="favico"><i class='fas fa-angle-down faico' style={{fontSize:"16px"}}></i></p></div>
+          </div>
+          <div id="paraico">
+          <div><p>How do I Cancel?</p><p class="favico"><i class='fas fa-angle-down faico' style={{fontSize:"16px"}}></i></p></div>
+          </div>
+          <div id="paraico">
+          <div><p>how does the Duo and Family Plan work? </p><p class="favico"><i class='fas fa-angle-down faico' style={{fontSize:"16px"}}></i></p></div>
+          </div>
+          <div id="paraico">
+          <div><p>How many devices  can I listen to?</p><p class="favico"><i class='fas fa-angle-down faico' style={{fontSize:"16px"}}></i></p></div>
+          </div>
+          <div id="paraico">
+          <div><p>How does the student Plan work?</p><p class="favico"><i class='fas fa-angle-down faico' style={{fontSize:"16px"}}></i></p></div>
+          </div>
+
+         </div>
+         
+      </div>
+  )
+}
