bf38c363f787da980c6036e9ce01b87b88416545
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 15:47:03 +0530

    component task2 of spotify

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import './Spotify/Component.css'
 import Component1 from './Spotify/Component1';
+import Component2 from './Spotify/Component2';
 function App() {
   return (
    <>
     <Component1></Component1>
+    <Component2></Component2>
    </>
   );
 }
---
+++ src/Spotify/Component2.js
@@ -0,0 +1,31 @@
+import React from 'react'
+ const Component2=()=> {
+  return (
+    <div id="outer1">
+      <div id="top"><h1 className="heading">The Power of Premium</h1></div>
+      <div id="bottom1">
+        <div id="divOne">
+            <img className="img_music" style={{height:"100px",width:"100px"}} src="https://www.pngitem.com/pimgs/m/268-2686285_woman-listening-to-music-listen-to-music-clipart.png" alt=""></img>
+            <h3>Ad-free Music listening</h3>
+            <p>Enjoy Uniterrupted music</p>
+        </div>
+        <div id="divtwo">
+            <img className="img_music" style={{height:"100px",width:"100px"}} src="https://www.technewstoday.com/wp-content/uploads/2022/06/how-to-play-offline-on-ps4-ps5.jpg" alt=""></img>
+            <h3>offline Playback</h3>
+            <p>Save your data by free listening</p>
+        </div>
+        <div id="divthree">
+            <img className="img_music"  style={{height:"100px",width:"100px"}} src="https://i0.wp.com/liveforlivemusic.com/wp-content/uploads/2016/02/musicbrain.jpg?fit=610%2C439&ssl=1" alt=""></img>
+            <h3>Play everywhere</h3>
+            <p>Listen on your Speaker, TV , and others favourite devices</p>
+        </div>  
+        <div id="divfour">
+            <img className="img_music" style={{height:"100px",width:"100px"}} src="https://i.pinimg.com/originals/e5/2b/fd/e52bfd6acb59a73d34b3371b8e39c265.png" alt=""></img>
+            <h3>Pay your way</h3>
+            <p>Prepare with paytm,UPI and more</p>
+        </div>
+      </div>
+   </div>
+  )
+}
+export default Component2;
