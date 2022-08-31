90ab10870d7c2618b2f219bd80ea0f19b56fa962
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 15:50:33 +0530

    component3 task of spotify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import './Spotify/Component.css'
 import Component1 from './Spotify/Component1';
 import Component2 from './Spotify/Component2';
+import Component3 from './Spotify/Component3';
 function App() {
   return (
    <>
     <Component1></Component1>
     <Component2></Component2>
+    <Component3></Component3>
    </>
   );
 }
